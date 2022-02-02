fd623efff622bd483963ebb9a3333fd2e36b342c
Tobby <oluwatobid07@gmail.com>
2022-02-02 23:47:28 +0100

    medical.sql added and .env file and code from internet to host on heroku

--- server.js
+++ server.js
@@ -3,6 +3,30 @@ const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
 
 
+//CODE FROM INERNET TO HOST ON HEROKU STARTS HERE
+const pg = require('pg');
+require('dotenv').config();
+
+// set production variable. This will be called when deployed to a live host
+const isProduction = process.env.NODE_ENV === 'production';
+
+// configuration details
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
+// if project has been deployed, connect with the host's DATABASE_URL
+// else connect with the local DATABASE_URL
+const pool = new pg.Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction,
+});
+
+// display message on success message if successful
+pool.on('connect', () => {
+  console.log('Teamwork Database connected successfully!');
+});
+
+//CODE FROM INERNET TO HOST ON HEROKU ENDS HERE
+
 const app = express();
 const mustache = mustacheExpress();
 const{Client} = require('pg');
